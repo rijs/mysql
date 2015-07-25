05400959447942954b5c87fa2adcf01dc88784e3
pemrouz <p3pedram@hotmail.com>
2015-07-25 19:34:23 +0100

    clear old tests

--- test.js
+++ test.js
@@ -1,27 +1,15 @@
 !(require('utilise/client')) && !function(){
 
 var expect = require('chai').expect
-  , client = require('utilise/client')
-  , resdir = require('./')
-  , path = require('path')
+  , mysql = require('./')
   , core = require('rijs.core')
   , css = require('rijs.css')
   , fn = require('rijs.fn')
  
-describe('Resources Folder', function(){
+describe('MySQL', function(){
 
-  it('should auto load resources folder', function(){  
-    var ripple = resdir(fn(css(core())))
-    expect(ripple('foo')).to.be.a('function')
-    expect(ripple('foo').name).to.eql('foo')
-    expect(ripple('bar.css')).to.equal('.bar {}')
-  })
+  it('should create correct insert SQL', function(){  
 
-  it('should auto load from specific dir', function(){  
-    var ripple = resdir(fn(css(core())), path.resolve())
-    expect(ripple('foo')).to.be.a('function')
-    expect(ripple('foo').name).to.eql('foo')
-    expect(ripple('bar.css')).to.equal('.bar {}')
   })
 
 })
